bb8b7dff8b8e8c9f3c007881d6ed8da6581c11fb
baj <>
2010-03-31 03:19:34 +0000

    fix

--- client/client.sh
+++ client/client.sh
@@ -4,7 +4,7 @@ PID_FILE="/tmp/lanmonitor_client.pid"
 PYTHON=`which python`
 
 if [ -f $PID_FILE ]; then
-    kill -9 `cat $PID_FILE`
+    kill -9 `cat $PID_FILE` || exit
 fi
 
 $PYTHON ./client.py &
