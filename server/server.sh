b0d352a532b5e02b04fea65cfc85392b2c2056d3
baj <>
2010-04-05 03:38:51 +0000

    fix

--- server/server.sh
+++ server/server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-HTML="index.html"
-PATTERN="192.168.*"
 LOG="log"
 PYTHON=`which python`
 PID_FILE="/tmp/lanmonitor_server.pid"
@@ -10,20 +8,11 @@ if [ -f $PID_FILE ]; then
     kill -9 `cat $PID_FILE` || exit
 fi
 
-cd /usr/local/bin/lanmonitor/server
-
-rm -f $HTML
-for i in $PATTERN; do
-   rm -f $i
-done
-
-./genhtml.sh
-
 $PYTHON -mSimpleHTTPServer 1>$LOG 2>&1 &
 PID1=$!
 
 sleep 1
-$PYTHON ./server.py &
+$PYTHON ./server.py 1>>$LOG 2>&1 &
 PID2=$!
 
 echo $PID1 $PID2 >$PID_FILE
